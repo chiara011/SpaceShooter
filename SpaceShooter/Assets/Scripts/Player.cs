80e7f27e6e950c5e8eecb83aae7d166fd5f2386b
chiara011 <36208248+chiara011@users.noreply.github.com>
2019-09-29 21:23:44 +0200

    Base 1

---
+++ SpaceShooter/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+ 
+    [SerializeField]
+    private float _speedHorizontal = 5f;
+    private float _speedVertical = 5f;
+
+    public float horizontalInput;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //take the current position to 0,0,0
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        _speedHorizontal = horizontalInput * 5;
+        _speedVertical = verticalInput * 5;
+
+        transform.Translate(Vector3.right * _speedHorizontal * Time.deltaTime);
+        transform.Translate(Vector3.up * _speedVertical * Time.deltaTime);
+
+        //transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime)
+        //or
+        //Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        //transform.Translate(direction * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 7.00)
+        {
+            Vector3 direction = new Vector3(transform.position.x, -14.00f, 0);
+            transform.Translate(direction);
+        }
+    }
+}
